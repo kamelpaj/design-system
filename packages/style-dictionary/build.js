b346bcae3cd4294b4ff62e1c6bc932e77923cf01
kamelpaj <qurridutt@gmail.com>
2022-09-06 16:31:40 +0200

    remove platform, add figma builds

--- packages/style-dictionary/build.js
+++ packages/style-dictionary/build.js
@@ -1,14 +1,17 @@
 const StyleDictionaryPackage = require("style-dictionary");
+const { transform } = require("@divriots/style-dictionary-to-figma");
 
 // HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
 
-function getStyleDictionaryConfig(brand, platform) {
+function getStyleDictionaryConfig(brand) {
   return {
-    source: [
-      `tokens/brands/${brand}/*.json`,
-      "tokens/globals/**/*.json",
-      `tokens/platforms/${platform}/*.json`,
-    ],
+    source: [`tokens/brands/${brand}/*.json`, "tokens/globals/**/*.json"],
+    format: {
+      figmaTokensPlugin: ({ dictionary }) => {
+        const transformedTokens = transform(dictionary.tokens);
+        return JSON.stringify(transformedTokens, null, 2);
+      },
+    },
     platforms: {
       web: {
         transformGroup: "web",
@@ -20,31 +23,13 @@ function getStyleDictionaryConfig(brand, platform) {
           },
         ],
       },
-      android: {
-        transformGroup: "android",
-        buildPath: `build/android/${brand}/`,
+      json: {
+        transformGroup: "js",
+        buildPath: `build/figma/${brand}/`,
         files: [
           {
-            destination: "tokens.colors.xml",
-            format: "android/colors",
-          },
-          {
-            destination: "tokens.dimens.xml",
-            format: "android/dimens",
-          },
-          {
-            destination: "tokens.font_dimens.xml",
-            format: "android/fontDimens",
-          },
-        ],
-      },
-      ios: {
-        transformGroup: "ios",
-        buildPath: `build/ios/${brand}/`,
-        files: [
-          {
-            destination: "tokens.h",
-            format: "ios/macros",
+            destination: "tokens.json",
+            format: "figmaTokensPlugin",
           },
         ],
       },
@@ -57,7 +42,7 @@ console.log("Build started...");
 // PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
 
 ["brand-1", "brand-2", "brand-3"].map(function (brand) {
-  ["web", "ios", "android"].map(function (platform) {
+  ["web", "json"].map(function (platform) {
     console.log("\n==============================================");
     console.log(`\nProcessing: [${platform}] [${brand}]`);
 

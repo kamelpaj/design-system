b51fdf9de2950f8df517254bd71a4ae8b3e345d8
kamelpaj <qurridutt@gmail.com>
2022-09-06 15:22:30 +0200

    multi brand dynamic scss

--- packages/acme-core/src/Button/index.tsx
+++ packages/acme-core/src/Button/index.tsx
@@ -1,12 +1,20 @@
 import * as React from "react";
-import styles  from "./Button.module.scss";
+import styles from "./Button.module.scss";
+import classNames from "classnames/bind";
 
 export interface ButtonProps {
   children: React.ReactNode;
+  brand: "brand-1" | "brand-2" | "brand-3";
 }
 
+let cx = classNames.bind(styles);
+
 export function Button(props: ButtonProps) {
-  return <button className={styles.btn}>{props.children}</button>;
+  let className = cx({
+    base: true,
+    [props.brand]: true
+  });
+  return <button className={className}>{props.children}</button>;
 }
 
 Button.displayName = "Button";
